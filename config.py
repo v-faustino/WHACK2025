fae1269a5935c142a8829aa98b97703fe7e8ee6a
Victor <vfaustino@proton.me>
2025-10-19 09:12:44 +0100

    Pls let this be over :sob:

---
+++ config.py
@@ -0,0 +1,21 @@
+import random
+
+BANKS = [
+    {"Name": "A", "Interest": 0.05, "Max Amount": 3000, "Time": 6 * 4},
+    {"Name": "B", "Interest": 0.03, "Max Amount": 2000, "Time": 8 * 4},
+    {"Name": "C", "Interest": 0.07, "Max Amount": 7600, "Time": 12 * 4},
+    {"Name": "D", "Interest": 0.10, "Max Amount": 10000, "Time": 24 * 4}
+]
+
+# Name : MSRP
+MERCHANDISE = {
+    "Soda": 1.99,
+    "Chocolate Bar": 1.49,
+    "Water": 0.99,
+    "Crisps": 0.99,
+    "Sweets": 1.49,
+    "Energy Drink": 2.99
+}
+
+MACHINECOST = 1000
+RUNNINGCOST = 20
