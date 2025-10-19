13ffea1ed0f464896a694bcb4f390a8756cd36ab
Umanga ‎ <sumanga45@gmail.com>
2025-10-19 01:03:32 +0100

    imma sleep

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from player import Player
+from config import MERCHANDISE, BANKS, MACHINECOST, RUNNINGCOST
 
 app = Flask(__name__)
 app.secret_key = "helloworld"
 
+# rn signifies the current time in weeks, beginning at week 1
+rn = 1
+gameOver = False
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -10,11 +15,16 @@ def login():
         return render_template("login.html")
     
     user = request.form.get("username")
-    if user == "":
-        return render_template("login.html", text="No fields may be left empty.")
+    '''if user == "":
+        return render_template("login.html", text="No fields may be left empty.")'''
 
     session["user"] = user
-    return render_template("index.html")
+
+    global player
+    player = Player(session["user"], 0) # Create player with no money
+
+    return redirect(url_for("home"))
+
 
 @app.route("/logout")
 def logout():
@@ -23,20 +33,50 @@ def logout():
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if not ("user" in session):
+    if not ("user" in session):   
         return redirect(url_for("login"))
     
 
     if request.method == "GET":
-        return render_template("index.html")
+        sold = player.getPrevWeekSales()
+        totalAmount = sum(int(sold[item][0]) for item in sold)
+        totalRev = sum(float(sold[item][1] * sold[item][0]) for item in sold)
+
+        return render_template("index.html", money="{:.2f}".format(player.getMoney()), time=rn, sold=sold, amount=totalAmount, rev="{:.2f}".format(totalRev))
 
 @app.route("/business", methods=["POST", "GET"])
 def business():
     if not ("user" in session):
         return redirect(url_for("login"))
     
+    if request.method == "GET":
+        return render_template("business.html", money="{:.2f}".format(player.getMoney()), time=rn, machines=player.getAmountMachines(), cost="{:.2f}".format(MACHINECOST), resources=player.getResources(), runningCost=player.getAmountMachines() * RUNNINGCOST)
 
-    return render_template("business.html")
+    # check empty
+    if request.form.get("amountMachines") == "" or request.form.get("newPrice") == "":
+        return render_template("business.html", money="{:.2f}".format(player.getMoney()), time=rn, machines=player.getAmountMachines(), cost=MACHINECOST, resources=player.getResources(), runningCost=player.getAmountMachines() * RUNNINGCOST, msg="Field cannot be empty")
+
+    # add machines or add resources price
+    if request.method == "POST":
+        if (request.form.get("newPrice") is None):
+            machines = int(request.form.get("amountMachines"))
+            
+            # check if you can afford 
+            if (player.getMoney() - machines * MACHINECOST) < 0:
+                msg = "You cannot afford that many vending machines"
+            
+            else:
+                player.addMachines(machines)
+                msg = f"Bought {machines} vending machines"
+        
+        else:
+            item = request.form.get("item")
+            price = request.form.get("newPrice")
+            player.editPrice(item, price)
+            msg = f"Updated price to {price}" 
+
+        return render_template("business.html", money="{:.2f}".format(player.getMoney()), time=rn, machines=player.getAmountMachines(), cost=MACHINECOST, resources=player.getResources(), runningCost=player.getAmountMachines() * RUNNINGCOST, msg=msg)
+
 
 @app.route("/employee", methods=["POST", "GET"])
 def employee():
@@ -44,7 +84,7 @@ def employee():
         return redirect(url_for("login"))
     
 
-    return render_template("employee.html")
+    return render_template("employee.html", money="{:.2f}".format(player.getMoney()), time=rn)
 
 @app.route("/personal", methods=["POST", "GET"])
 def personal():
@@ -52,39 +92,144 @@ def personal():
         return redirect(url_for("login"))
     
 
-    return render_template("personal.html")
+    return render_template("personal.html", money="{:.2f}".format(player.getMoney()), time=rn)
 
 @app.route("/vendors", methods=["POST", "GET"])
 def vendors():
     if not ("user" in session):
         return redirect(url_for("login"))
     
+    if request.method == "GET":
+        return render_template("vendors.html", money="{:.2f}".format(player.getMoney()), time=rn, stock=MERCHANDISE, price=player.getCurrentPrices())
+    
 
-    return render_template("vendors.html")
+    name = request.form.get("name")
+    amount = request.form.get("amountResources")
+    price = float(request.form.get("price"))
+
+    if amount == "":
+        return render_template("vendors.html", money="{:.2f}".format(player.getMoney()), time=rn, stock=MERCHANDISE, price=player.getCurrentPrices(), msg="Fields cannot be left empty")
+
+    amount = int(amount)
+
+    # check if can afford
+    if (player.getMoney() - amount * price) < 0:
+        return render_template("vendors.html", money="{:.2f}".format(player.getMoney()), time=rn, stock=MERCHANDISE, price=player.getCurrentPrices(), msg="You cannot afford these resources")
+
+    player.addResources(name, amount, price)
+
+    return render_template("vendors.html", money="{:.2f}".format(player.getMoney()), time=rn, stock=MERCHANDISE, price=player.getCurrentPrices(), msg2="Successful purchase")
 
 
 @app.route("/banks", methods=["POST", "GET"])
 def banks():
     if not ("user" in session):
-        return redirect(url_for("login"))
+        return redirect(url_for("logout"))
     
 
     if request.method == "GET":
-        return redirect(url_for("login"))
+        return render_template("banks.html", loans=player.getLoans(), banks=BANKS, money="{:.2f}".format(player.getMoney()), time=rn)
+
     
 
     amount = request.form.get("amount")
     
     # no fields can be empty
     if amount == "":
-        return render_template("banks.html", text="No fields may be left empty.")
+        return render_template("banks.html", text="No fields may be left empty.", loans=player.getLoans(), banks=BANKS, money="{:.2f}".format(player.getMoney()), time=rn)
     
 
     # loan approved 
     # add amoun to account 
+    player.addMoney(float(amount))
+    
+    bank = BANKS[int(request.form.get("index"))]
+
+    interest = bank["Interest"]
+    time = bank["Time"]
+    name = bank["Name"]
+
+    print(name, interest, time)
+
+    # existingLoans = player.getLoans()
+
+    player.addLoan(name, amount, interest, time, rn)
+    print(player.getLoans())
+    return render_template("banks.html", text2="Loan Approved.", loans=player.getLoans(), banks=BANKS, money="{:.2f}".format(player.getMoney()), time=rn)
 
 
-    return render_template("banks.html", text2="Loan Approved.")
+@app.route("/advance", methods=["POST", "GET"])
+def advanceTime():
+    loanStatus = player.forwardLoans()
+    saleStatus = player.sales()
+    if loanStatus == "Game Over" or saleStatus == "Game Over": 
+        flash("Unfortuntately you failed to pay credit in time. Game Over.", 'danger')
+
+    else :
+        global rn
+        rn += 1
+
+        player.currentPrices = None # Reset Current Prices, so new ones are generated next week
+        
+        totalRunningCost = player.getAmountMachines() * RUNNINGCOST
+        if totalRunningCost > player.getMoney():
+            moneh = player.getMoney()
+            player.addMoney(-moneh) # Zero Money
+            player.addLoan("Machine Maintenance", totalRunningCost - moneh, 0.30, 0, rn) # Add difference as a loan
+
+            #TODO ADD MESSAGE ABOUT THIS
+            flash("You didn't have enough money to run all your machines, emergency loan taken. PAY IMMEDIATELY!!!", 'danger')
+            
+            #TODO DISPLAY WEEKLY RUNNING COST IN BUSINESS TAB IG??? 
+
+        else:
+            player.addMoney(-totalRunningCost) # Every week, each machine costs some money
+
+        # Overdue
+        if loanStatus == "Overdue":
+            flash("Payment for a loan was overdue, your interest has been increased.",'error')
+
+        # Game Over
+        elif loanStatus == "Game Over": 
+            flash("Game Over. You missed too many payment dates.",'error')
+
+        elif saleStatus == "Not Enough Goods":
+            flash("You didn't have enough stock to satisfy all your customers.", 'warning')
+
+        elif saleStatus == "Not Diverse":
+            flash("You don't have enough unique items. Sales severely impacted.", 'warning')
+
+        elif saleStatus == "No Machines": 
+            flash("You didn't have any machines working.", 'warning')
+
+        else:
+            flash("1 week advanced, loans are normal.", 'success')
+
+
+
+    return redirect(url_for("home"))
+
+
+@app.route("/repay", methods=["POST", "GET"])
+def repay():
+    bank = request.form.get("bank")
+    amount = request.form.get("repayAmount")
+    msg = ""
+    msg2 = ""
+    if amount == "":
+        msg = "Fields cannot be empty"
+
+    if amount is None:
+        msg = "Try again"
+
+    elif float(amount) <= player.getMoney():
+        player.payLoan(bank, amount)
+        msg2 = "Payment made successfully"
+
+    else:
+        msg="You cannot afford to make this payment."
+
+    return render_template("banks.html", msg=msg, msg2=msg2, loans=player.getLoans(), banks=BANKS, money="{:.2f}".format(player.getMoney()), time=rn)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ player.py
@@ -0,0 +1,246 @@
+import json
+from os import path
+import random
+import math
+from copy import deepcopy
+from config import MERCHANDISE, MACHINECOST
+
+class Player:
+    # data = {
+    #     "loans": [{"Name": "Bank", "Amount": 1000, "Interest": 0.05, "Payment Date": "01-01-2025", "Time Left": 12}],
+    #     "machines": {"Amount": 13, "Resources": {"coca": (500, 2.99)}, "prevSales": {"coca": (200,2.99)},}
+    # }
+
+    data = {"loans": [], "machines": {"Amount": 0, "Resources": {}, "Previous Week": {}}}
+    currentPrices = None
+
+    def __init__(self, name, money):
+        self.name = name
+        self.data["money"] = money
+
+        try:
+            self.data = json.loads(self.getUserData())
+        except:
+            self.storeUserData()
+
+    def getAmountMachines(self):
+        self.data = json.loads(self.getUserData())
+        return self.data["machines"]["Amount"]
+
+    def getResources(self):
+        self.data = json.loads(self.getUserData())
+        
+        return self.data["machines"]["Resources"]
+    
+    def addResources(self, name, amount, price):
+        amount = int(amount)
+        price = float(price)
+
+        resources = self.getResources()
+
+        if name in resources:
+            val0 = resources[name][0] + amount
+            val1 = resources[name][1]
+        else:
+            val0 = amount
+            val1 = MERCHANDISE[name]
+
+        resources[name] = [val0, val1]
+        self.data = json.loads(self.getUserData())
+        self.data["machines"]["Resources"] = resources
+        self.storeUserData()
+
+        self.addMoney(-price)
+
+    def editPrice(self, name, newPrice):
+        resources = self.getResources()
+        
+        resources[name] = [resources[name][0], newPrice]
+        self.data = json.loads(self.getUserData())
+        self.data["machines"]["Resources"] = resources
+        self.storeUserData()
+
+    def addMachines(self, numberAdded):
+        self.data = json.loads(self.getUserData())
+        self.data["machines"]["Amount"] += int(numberAdded)
+        self.storeUserData()
+
+        self.addMoney(-MACHINECOST)
+
+    # Returns Name : Current Sales Value
+    def getCurrentPrices(self):
+        if self.currentPrices is not None:
+            return self.currentPrices
+
+        output = MERCHANDISE
+        for name in MERCHANDISE:
+            output[name] = round((1 + ((random.random() - 0.5) * 3.5/5)) * (MERCHANDISE[name] * 3/4), 2)
+        
+        self.currentPrices = output
+        return output
+
+    def addMoney(self, amount):
+        self.data = json.loads(self.getUserData())
+        self.data["money"] += amount
+        self.storeUserData()
+
+
+    def getMoney(self):
+        self.data = json.loads(self.getUserData())
+        return self.data["money"]
+
+    def addLoan(self, name, amount, interest, time, rn):
+        amount = int(amount)
+        interest = float(interest)
+        self.data = json.loads(self.getUserData())
+        self.data["loans"].append({"Name": name, "Amount": amount, "Interest": interest, "Payment Date": 4})
+        self.storeUserData()
+
+    def getLoans(self):
+        self.data = json.loads(self.getUserData())
+        return self.data["loans"]
+    
+    def getMachines(self):
+        self.data = json.loads(self.getUserData())
+        return self.data["machines"]
+
+    def setMachines(self, machines):
+        self.data = json.loads(self.getUserData())
+        self.data["machines"] = machines
+        self.storeUserData()
+
+    def storeUserData(self):
+        with open(path.join("data", f"{self.name}.json"), "w") as f:
+            f.write(json.dumps(self.data))
+
+    def getUserData(self):
+        with open(path.join("data", f"{self.name}.json"), "r") as f:
+            # Only 1 line so its fine
+            for line in f:
+                return line
+
+    def forwardLoans(self):
+        loans = self.getLoans()
+        self.data = json.loads(self.getUserData())
+
+        # Bad coding, ik ik
+
+        status = "Normal"
+
+        for i in range(len(loans)):
+            loans[i]["Payment Date"] -= 1
+            
+            # If 4 weeks late, you lose!
+            if (loans[i]["Payment Date"]) == -4:
+                status = "Game Over"      
+            # If payment overdue, increase interest
+            elif (loans[i]["Payment Date"]) <= -1:
+                loans[i]["Interest"] += 0.02
+                status = "Overdue"
+
+            print(loans)
+            loans[i]["Amount"] *= (1 + loans[i]["Interest"] / 12) 
+
+            # Hard round here cuz i cba to find where to round in proper place
+            loans[i]["Amount"] = round(loans[i]["Amount"], 2)
+            loans[i]["Interest"] = round(loans[i]["Interest"], 2)
+
+        self.data["loans"] = loans
+        self.storeUserData()
+        return status
+
+    def sales(self):
+        status = "Normal"
+        machines = self.getMachines()
+        numMachines = machines["Amount"]
+        resources = machines["Resources"]
+
+        if numMachines == 0:
+            return "No Machines"
+
+        # Visiblity with a random deviation of +- 20%
+        vis = (1 + (random.random() - 0.5) / 2.5) * getVisibility(numMachines)
+        vis = int(vis)
+
+        # Factor variety in
+        vis *= len(resources) / len(MERCHANDISE)
+
+        prevWeek = {}
+        copy = deepcopy(resources)
+
+        print(resources)
+
+        for name in copy:
+            numberAvailable, cost = resources[name]
+            numberAvailable = int(numberAvailable)
+            cost = float(cost)
+
+            roughSales = vis / len(resources)
+
+            # Stinker line - sells the rough number of sales +- 0.2
+            actualSales = int(purchaseChance(MERCHANDISE[name], cost) * (roughSales + random.randint(int(-0.2*roughSales), int(0.2*roughSales))))
+
+            # Altho that could surpass the number available, so its limited to the availability
+            sales = min(numberAvailable, actualSales)
+            
+            if actualSales > numberAvailable:
+                status = "Not Enough Goods"
+                del resources[name]
+            else:
+                resources[name] = numberAvailable - sales, cost
+
+            moneh = round(cost * sales, 2)
+            self.addMoney(moneh)
+
+            prevWeek[name] = [sales, round(cost, 2)]
+
+        machines["Resources"] = resources
+        machines["Previous Week"] = prevWeek
+        self.setMachines(machines)
+
+        if len(resources) / len(MERCHANDISE) != 1:
+            status = "Not Diverse"
+
+        return status
+
+    def getPrevWeekSales(self):
+        machines = self.getMachines()
+        return machines["Previous Week"]
+
+    def payLoan(self, name, amount):
+        amount = float(amount)
+        self.data = json.loads(self.getUserData())
+
+        totalMoney = 0
+        for loan in self.data["loans"]:
+            if loan["Name"] == name:
+                loan["Amount"] -= amount
+                totalMoney += amount
+
+                print(loan) 
+                break
+
+        print(self.data["loans"])
+
+        # Should be fine cuz 1 should be 0 at a time?
+        copy = deepcopy(self.data["loans"])
+        for index, loan in enumerate(copy):
+            if loan["Amount"] == 0:
+                self.data["loans"].pop(index)
+
+        self.storeUserData()
+        self.addMoney(-totalMoney)
+
+def purchaseChance(expectedPrice, setPrice):
+    STEEPNESS = 1.3
+
+    return 0.5 + (math.atan(-STEEPNESS * (setPrice - expectedPrice)) / math.pi)
+
+def getVisibility(numMachines):
+    VISPERMACHINE = 300
+
+    return VISPERMACHINE*numMachines + 20 * (numMachines - 1) ** 2
+
+
+
+
